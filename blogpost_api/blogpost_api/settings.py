b3f82e73e79675b67df2119dd9059ce01db54deb
Rafal <r.bogusz2606@gmail.com>
2024-11-26 18:19:10 +0100

    settings changes

--- blogpost_api/blogpost_api/settings.py
+++ blogpost_api/blogpost_api/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # Adres frontendu
-    'https://django-react-blogpost-app.vercel.app/',
+    'https://django-react-blogpost-app.vercel.app',
 ]
 
 ROOT_URLCONF = 'blogpost_api.urls'
